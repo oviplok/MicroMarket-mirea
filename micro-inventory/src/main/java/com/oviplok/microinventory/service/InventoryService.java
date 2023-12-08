369e7db5cecbf7bae910754199670dddc29ada21
Nikita <hahehm@mail.ru>
2023-12-08 13:24:44 +0300

    Implemented Circuit Breaker

--- micro-inventory/src/main/java/com/oviplok/microinventory/service/InventoryService.java
+++ micro-inventory/src/main/java/com/oviplok/microinventory/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.oviplok.microinventory.dto.InventoryResponse;
 import com.oviplok.microinventory.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Checking Inventory");
+//        log.info("Wait started");
+//        Thread.sleep(10000);
+//        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
