38b7150e353e21ee757485f18f65c19af596eebb
Nikita <hahehm@mail.ru>
2024-01-18 13:54:47 +0300

    YC gateway update

--- api-gateway/src/main/java/config/CustomContainer.java
+++ api-gateway/src/main/java/config/CustomContainer.java
@@ -4,14 +4,14 @@ import org.springframework.boot.web.server.*;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.stereotype.Component;
 
-@Component
-public class CustomContainer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-
-    @Override
-    public void customize(ConfigurableServletWebServerFactory factory) {
-        String port = System.getenv("PORT"); // Получаем порт из переменной окружения
-        if (port != null) {
-            factory.setPort(Integer.parseInt(port)); // Устанавливаем порт для фабрики сервлетов
-        }
-    }
-}
+//@Component
+//public class CustomContainer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+//
+//    @Override
+//    public void customize(ConfigurableServletWebServerFactory factory) {
+//        String port = System.getenv("PORT"); // Получаем порт из переменной окружения
+//        if (port != null) {
+//            factory.setPort(Integer.parseInt(port)); // Устанавливаем порт для фабрики сервлетов
+//        }
+//    }
+//}
